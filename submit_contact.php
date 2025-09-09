84bdbab3baf618464b3c02b86a4762334325b464
Beïza Cosgun <bcosgun781@gmail.com>
2025-09-09 10:50:05 +0200

    Controlez la valeur des parametres

--- submit_contact.php
+++ submit_contact.php
@@ -6,7 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Confirmation</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
 </head>
 <body class="d-flex flex-column min-vh-100">
 <div class="container">
@@ -14,12 +17,15 @@
     <?php include_once(__DIR__ . '/header.php'); ?>
 
     <h1>Message bien reçu !</h1>
-    
+
     <?php
-    // Verification pour savoir si il ya le mail et le messag ede present
-    if (!isset($_GET['email']) || !isset($_GET['message'])) {
-        echo "<p>Il faut un email et un message pour soumettre le formulaire.</p>";
-        return; 
+    // Contrôle des paramètres : email valide + message non vide
+    if (
+        (!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL))
+        || (!isset($_GET['message']) || empty($_GET['message']))
+    ) {
+        echo "Il faut un email et un message valides pour soumettre le formulaire.";
+        return; // on arrête l’exécution
     }
     ?>
 
