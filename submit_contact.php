0545fdc2eff6b046436babc929ea9804cfac2b2f
Beïza Cosgun <bcosgun781@gmail.com>
2025-09-09 11:04:33 +0200

    Securisez votre code

--- submit_contact.php
+++ submit_contact.php
@@ -1,3 +1,4 @@
+
 <!-- submit_contact.php -->
 <!DOCTYPE html>
 <html>
@@ -6,34 +7,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de recettes - Confirmation</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="d-flex flex-column min-vh-100">
 <div class="container">
 
     <?php include_once(__DIR__ . '/header.php'); ?>
 
-    <h1>Message bien reçu !</h1>
-
     <?php
-    // Contrôle des paramètres : email valide + message non vide
     if (
         (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
         || (!isset($_POST['message']) || empty($_POST['message']))
     ) {
         echo "Il faut un email et un message valides pour soumettre le formulaire.";
-        return; // on arrête l’exécution
+        return; 
     }
+
+   
+    $safeEmail = htmlspecialchars($_POST['email']);
+    $safeMessage = htmlspecialchars($_POST['message']);
     ?>
 
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Rappel de vos informations</h5>
-            <p class="card-text"><b>Email</b> : <?php echo $_POST['email']; ?></p>
-            <p class="card-text"><b>Message</b> : <?php echo $_POST['message']; ?></p>
+            <p class="card-text"><b>Email</b> : <?php echo $safeEmail; ?></p>
+            <p class="card-text"><b>Message</b> : <?php echo $safeMessage; ?></p>
         </div>
     </div>
 
